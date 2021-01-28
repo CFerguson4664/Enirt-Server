f9265a4877e230e6255dd769f9405f748ab82b0a
Moscovium <michael.swango@gmail.com>
2021-01-27 21:59:26 -0500

    Cool Color Visual
    It's neat

--- Enirt-Test-Client/Assets/Scripts/Menu/loadGame.cs
+++ Enirt-Test-Client/Assets/Scripts/Menu/loadGame.cs
@@ -7,26 +7,34 @@ using UnityEngine.UI;
 public class loadGame : MonoBehaviour
 {
     static string IP;
+    public Slider colorSlider;
     public static string errorText = "";
 
     // Start is called before the first frame update
     void Start()
     {
+        colorValueChange();
         GameObject.Find("Error").GetComponent<UnityEngine.UI.Text>().text = errorText;
+        colorSlider.onValueChanged.AddListener(delegate { colorValueChange(); });
     }
+
+    public void colorValueChange()
+    {
+        float sliderValue = colorSlider.value;
+        float red = 1.0f - sliderValue;
+        float green = 0.0f + sliderValue;
+        manager.clientColor = new Color(red, green, 0.0f);
+
+        colorSlider.gameObject.transform.Find("Background").GetComponent<Image>().color = manager.clientColor;
+        colorSlider.gameObject.transform.Find("Fill Area").Find("Fill").GetComponent<Image>().color = manager.clientColor;
+    }
+
 
     public void StartGame()
     {
         //Set necessary parameters and opens the game scene
         PlayerMovement.keyboardEnable = GameObject.Find("KeyboardToggle").GetComponent<Toggle>().isOn;
         manager.clientName = GameObject.Find("CharacterName").GetComponent<InputField>().text;
-
-        float sliderValue = GameObject.Find("CharacterColorSlider").GetComponent<Slider>().value;
-        float red = 1.0f - sliderValue;
-        float green = 0.0f + sliderValue;
-        manager.clientColor = new Color(red, green, 0.0f);
-
-        Debug.Log(manager.clientName);
 
         netComs.IPAddress = IP;
         SceneManager.LoadScene("game");
